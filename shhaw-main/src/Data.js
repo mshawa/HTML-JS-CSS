4c62f01af7d338618eb64f2fb8f47bdc579f9c62
mshawa <39690188+mshawa@users.noreply.github.com>
2023-01-16 08:54:06 +0300

    Add files via upload

---
+++ shhaw-main/src/Data.js
@@ -0,0 +1,48 @@
+
+
+
+const shopItemsData = [
+  {
+    id: "thyfhcbcv",
+    name: "C4 pre-workout",
+    price: 300,
+    desc: "Take your C4 and smash your gym",
+    img: "images/c4.jpeg",
+  },
+ {
+    id: "tddd",
+    name: "SCULPTnation-preworkout",
+    price: 30,
+    desc: "Full of caffine =full of power.",
+    img: "images/sculp.jpeg",},
+
+    {id: "tddhyud",
+    name: "Serious mass",
+    price: 30,
+    desc: "super gainer to add a lot of kgs to your wight .",
+    img: "images/R.jpeg",},
+
+    {id: "tduydud",
+    name: "Whey protien",
+    price: 30,
+    desc: "Get your protein with low carb & fat .",
+    img: "images/OIP.jpeg"},
+
+    {id: "tuydud",
+    name: "short",
+    price: 30,
+    desc: "get your shorts from us .",
+    img: "images/shots.jpeg",},
+    
+    {id: "tud",
+    name: "big offer",
+    price: 100,
+    desc: "Don't miss this  .",
+    img: "images/10453926445106.jpg",
+ 
+ 
+
+  }
+];
+
+
